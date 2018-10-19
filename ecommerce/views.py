83cb128f5e245cb4e7c7afc84798bcd5102f41f0
sina <ebr.sina@gmail.com>
2018-10-19 06:36:21 +0330

    add item to content lib in html

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,12 +4,24 @@ from django.shortcuts import render
 
 def home(request):
     # return  HttpResponse("hello world")
-    return render(request, 'home_page.html')
+    context = {
+        "title":"home",
+        "content":"from home page",
+    }
+    return render(request, 'home_page.html' , context)
 
 def about(request):
     # return  HttpResponse("hello world")
-    return render(request, 'home_page.html')
+    context = {
+        "title":"about",
+        "content": "from about page",
+    }
+    return render(request, 'home_page.html' , context)
 
 def contact(request):
     # return  HttpResponse("hello world")
-    return render(request, 'home_page.html')
+    context = {
+        "title":"contact",
+        "content": "from about contact",
+    }
+    return render(request, 'home_page.html' , context)
